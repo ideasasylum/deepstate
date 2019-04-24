0da9bc35883b5ad0596e2899985b9a1a92f1fd56
Jamie Lawrence <jamie@ideasasylum.com>
2019-04-24 21:54:15 +0100

    How now brown cow?

--- lib/deep_state.rb
+++ lib/deep_state.rb
@@ -1,9 +1,9 @@
 require "deep_state/version"
 require "deep_state/module"
+require "deep_state/state_machine"
 require "deep_state/state_definition"
 require "deep_state/hook"
 require "deep_state/event"
-require "deep_state/state_machine"
 
 module DeepState
   class Error < StandardError; end
