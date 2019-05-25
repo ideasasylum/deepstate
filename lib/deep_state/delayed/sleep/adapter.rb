57ab40ee863d46c4809b39e66023d1392ae867a3
Jamie Lawrence <jamie@ideasasylum.com>
2019-05-25 01:05:11 +0100

    Only continue with a delayed trnasition if the state hasn’t changed

--- lib/deep_state/delayed/sleep/adapter.rb
+++ lib/deep_state/delayed/sleep/adapter.rb
@@ -4,9 +4,13 @@ module DeepState
       class Adapter
         def self.call machine, event
           existing_state = machine.current_state
+          last_modification = machine.updated_at
 
           sleep event.delay
 
+          # Cancel this transition if the machine state was already changed
+          raise DeepState::Error if machine.updated_at > last_modification
+          # Cancel the transition if it's no longer valid
           raise DeepState::Error unless machine.can?(event.name)
 
           machine.execute_transition event
